0aabb909c97fc229b942da1c1fef7cd2879b6ec6
trambert <trambert92@gmail.com>
2021-09-13 00:09:15 -0400

    updates

--- dist/index.js
+++ dist/index.js
@@ -9396,9 +9396,9 @@ const run = async () => {
         const ownerrepository = core.getInput('repository')
     
         const repoOwner = (ownerrepository.substring(0, ownerrepository.indexOf('/')))
-        const repo = ownerrepository.substring(ownerrepository.indexOf('/') + 1)
-        console.log('OWNER:', repoOwner)
-        console.log('REPOSITORY:', repo)
+        const repository = ownerrepository.substring(ownerrepository.indexOf('/') + 1)
+        // console.log('OWNER:', repoOwner)
+        // console.log('REPOSITORY:', repo)
         const octokit = github.getOctokit(token)
         // const latestTag = execSync('git describe --tags --abbrev=0', (error, stdout, stderr) => {
         //     if (error) {
@@ -9431,7 +9431,7 @@ const run = async () => {
         // const { context = {} } = github
         // console.log(context.repository)
         // const { owner: currentOwner, name: currentRepo } = context.repository
-        const results = await octokit.rest.repos.listTags({ owner: repoOwner, repository: repo })
+        const results = await octokit.rest.repos.listTags({ owner: repoOwner, repo: repository, })
         console.log('OCTOKIT:', results)
         console.log(token, defaultBump, withV)
     } catch (error) {
