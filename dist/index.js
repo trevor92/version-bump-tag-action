81df5edfa6fefa71f2c9c0cb0a39c8bd7e0a1d90
trambert <trambert92@gmail.com>
2021-09-13 14:05:19 -0400

    updates

--- dist/index.js
+++ dist/index.js
@@ -9397,43 +9397,14 @@ const run = async () => {
     
         const repoOwner = (ownerrepository.substring(0, ownerrepository.indexOf('/')))
         const repository = ownerrepository.substring(ownerrepository.indexOf('/') + 1)
-        // console.log('OWNER:', repoOwner)
-        // console.log('REPOSITORY:', repo)
         const octokit = github.getOctokit(token)
-        // const latestTag = execSync('git describe --tags --abbrev=0', (error, stdout, stderr) => {
-        //     if (error) {
-        //         console.log(`error: ${error.message}`);
-        //         return;
-        //     }
-        //     if (stderr) {
-        //         console.log(`stderr: ${stderr}`);
-        //         return;
-        //     }
-        //     return process.stdout.write(stdout);
-            // console.log(`stdout: ${stdout}`);
-        // })
-        // console.log('LATEST_TAG', latestTag)
-
-        // const commitsSinceLastTag = execSync(`git log ${latestTag}..HEAD --pretty=format:"%s"'`, (error, stdout, stderr) => {
-        //     if (error) {
-        //         console.log(`error: ${error.message}`)
-        //         return
-        //     }
-        //     if (stderr) {
-        //         console.log(`stderr: ${stderr}`)
-        //         return
-        //     }
-        //     return stdout;
-        // })
-
-        // console.log(commitsSinceLastTag)
-        // console.log(octokit)
-        // const { context = {} } = github
-        // console.log(context.repository)
-        // const { owner: currentOwner, name: currentRepo } = context.repository
         const results = await octokit.rest.repos.listTags({ owner: repoOwner, repo: repository, })
-        console.log('OCTOKIT:', results)
-        console.log(token, defaultBump, withV)
+        const repoTags = results.data.map(tag => tag.name)
+        console.log(repoTags)
+        const latestTag = semver.sort(repoTags)
+        console.log(latestTag[0])
+        // console.log('OCTOKIT:', results.data)
+        
     } catch (error) {
         console.log('ERROR:', error)
       core.setFailed(error.message);
