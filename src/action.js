cda9f3a6db8c55b13a77a6c8fa8274a9f1731f2b
trambert <trambert92@gmail.com>
2021-09-21 19:49:07 -0400

    updates

--- src/action.js
+++ src/action.js
@@ -45,13 +45,13 @@ const run = async () => {
         const releaseRules = [
             {type: 'PATCH', release: 'patch'}, {type: 'MINOR', release: 'minor'}, {type: 'MAJOR', release: 'major'}
         ]
-        // console.log(commitsSinceLastTag.data.commits[0])
+        console.log(commitsSinceLastTag.data.commits)
 
         for( c of commitsSinceLastTag.data.commits) {
-            // console.log(c.commit.message)
-            if(c.commit.message == undefined) {
-                console.log(c)
-            }
+            console.log(c.commit.message)
+            // if(c.commit.message == undefined) {
+            //     console.log(c)
+            // }
             // requestedBump = await analyzeCommits(releaseRules, c.commit.message)
             // console.log(requestedBump)
             // const message = c.commit.message
